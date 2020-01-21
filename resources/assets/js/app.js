3a0a3c8d76f8fa65e572aecd2a1627fad72ea5ba
Pharoouzy <pharoouzy@yahoo.com>
2020-01-21 11:12:57 +0100

    Import all vuejs libraries and vue components created
    The libraries include vue-router, vue-axios, and axios. The vomponents are Login, Register, and Dashboard

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App'
 import Home from './components/Home'
+import Dashboard from './components/Dashboard'
+import Register from './components/Register'
+import Login from './components/Login'
 
 Vue.use(VueRouter)
 
