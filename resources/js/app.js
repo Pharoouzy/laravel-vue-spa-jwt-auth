d26fd673bfc5591613c24144d1ca1b2f3117bd34
Pharoouzy <pharoouzy@yahoo.com>
2020-01-21 10:27:15 +0100

    Add Home.vue and setup app.js
    Home.vue is a vuejs template that will be used to render the content of the home page. I import vue and vue-router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-require('./bootstrap');
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App'
+import Home from './components/Home'
+
+Vue.use(VueRouter)
+
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Laravel Vue SPA Authentication</h1>
+</template>
