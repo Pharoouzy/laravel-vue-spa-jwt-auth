687eea28444e1f18cceb4a67eb108802672b2ef8
Pharoouzy <pharoouzy@yahoo.com>
2020-01-21 15:03:29 +0100

    Confugure logout route in api.php

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('auth/login', 'AuthController@login');
 
 Route::group(['middleware' => 'jwt.auth'], function(){
-  Route::get('auth/user', 'AuthController@user');
+    Route::get('auth/user', 'AuthController@user');
+    Route::post('auth/logout', 'AuthController@logout');
 });
 
 Route::group(['middleware' => 'jwt.refresh'], function(){
-  Route::get('auth/refresh', 'AuthController@refresh');
+    Route::get('auth/refresh', 'AuthController@refresh');
 });
