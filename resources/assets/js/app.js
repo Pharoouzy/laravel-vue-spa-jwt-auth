a11a0031ba92f26943f525bde85e6129d9f5c7ba
Pharoouzy <pharoouzy@yahoo.com>
2020-01-21 11:35:09 +0100

    Configure vue-auth further

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -50,6 +50,19 @@ const router = new VueRouter({
 
 Vue.router = router
 
+Vue.use(require('@websanova/vue-auth'), {
+    //configures vue-auth to use the bearer driver which basically adds the authentication token to the request header during requests and reads and parses the token from server responses.
+    auth: require('@websanova/vue-auth/drivers/auth/bearer.js'),
+    // configures vue-auth to use the axios http driver, since I am using axios for http requests.
+    http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
+    // configures vue-auth to use the driver for vue-router since that is what I am using.
+    router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js')
+})
+
+App.router = router
+
+new Vue(App).$mount('#app')
+
 new Vue({
     el: '#app',
     router: router,
