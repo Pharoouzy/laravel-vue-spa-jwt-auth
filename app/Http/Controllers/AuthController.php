14727aff97cf9635a3c1252959647a1c13389a76
Pharoouzy <pharoouzy@yahoo.com>
2020-01-21 14:00:36 +0100

    Create register method in AuthController
    This method is for handling user registration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,4 +6,15 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller {
     //
+    public function register(RegisterFormRequest $request) {
+        $user = new User;
+        $user->email = $request->email;
+        $user->name = $request->name;
+        $user->password = bcrypt($request->password);
+        $user->save();
+        return response([
+            'status' => 'success',
+            'data' => $user
+        ], 200);
+    }
 }
