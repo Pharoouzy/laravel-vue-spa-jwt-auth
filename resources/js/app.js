a61e0428d8155b40589ea794c8299795747dae62
Pharoouzy <pharoouzy@yahoo.com>
2020-01-21 10:36:36 +0100

    Configure Vue and VueRouter

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,18 @@ import Home from './components/Home'
 
 Vue.use(VueRouter)
 
+const router = new VueRouter({
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        }
+    ]
+})
+
+new Vue({
+    el: '#app',
+    router: router,
+    render: app => app(App)
+})
