a5e83020c614a95cbd4023e7495747bc3f427875
Pharoouzy <pharoouzy@yahoo.com>
2020-01-22 13:03:39 +0100

    Fix authentication issue

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,6 +12,17 @@ use App\Http\Requests\RegisterFormRequest;
 
 class AuthController extends Controller {
     //
+    
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+
     public function register(RegisterFormRequest $request) {
         $user = new User;
         $user->email = $request->email;
@@ -24,15 +35,24 @@ class AuthController extends Controller {
         ], 200);
     }
 
+
+    /**
+     * Get a JWT token via given credentials.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function login(Request $request) {
         $credentials = $request->only('email', 'password');
         $token = null;
         if (!$token = JWTAuth::attempt($credentials)) {
-            return response([
-                'status' => 'error',
-                'error' => 'invalid.credentials',
-                'msg' => 'Invalid Credentials.'
-            ], 400);
+            return $this->respondWithToken($token);
+            // return response([
+            //     'status' => 'error',
+            //     'error' => 'invalid.credentials',
+            //     'msg' => 'Invalid Credentials.'
+            // ], 400);
         }
         return response([
             'status' => 'success'
@@ -64,5 +84,29 @@ class AuthController extends Controller {
             'status' => 'success',
             'msg' => 'Logged out Successfully.'
         ], 200);
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token) {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => $this->guard()->factory()->getTTL() * 60
+        ]);
+    }
+
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return \Illuminate\Contracts\Auth\Guard
+     */
+    public function guard() {
+        return Auth::guard();
     }
 }
